516ccc06ccafc51d0a3d51c9f80383c42680a3ed
ssenerg <amirhossein.pdn@gmail.com>
2023-07-07 15:11:58 +0330

    fibonacci heap is added

--- tmp2.py
+++
@@ -1,7 +0,0 @@
-from HAC.heaps.max_fibonacci import MaxFibonacciHeap
-a = MaxFibonacciHeap()
-for i in range(0, 100000):
-    a.push(i, i + 1)
-print(a.find_max.key)
-for i in range(0, 100000):
-    a.pop()
